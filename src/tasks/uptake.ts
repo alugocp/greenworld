dd1bc9b982b757052f8c321b19e1a83b8976b1f3
alugocp@aim.com <alugocp@aim.com>
2021-11-13 23:51:47 -0500

    Upgraded a task to print more crop data

--- src/tasks/uptake.ts
+++ src/tasks/uptake.ts
@@ -7,11 +7,17 @@ import * as fs from 'fs';
 const data: object = JSON.parse(fs.readFileSync('database.json').toString());
 
 for (const plant of data['plants']) {
-  console.log(`${plant.name} (${plant.species})`);
-  const table = {...data['conversions'], ...(plant.conversions || {})};
-  for (const nutrient of plant.uptake) {
-    const ratio = conversions.convertUptakeUnits(nutrient.rate, table);
-    console.log(`${nutrient.nutrient}: ${nutrient.rate} (${ratio} units/biomass)`);
+  process.stdout.write(`\x1b[1;32m\x1b[4;32m${plant.name}\x1b[0m - ${plant.species}\n`);
+  if (plant.pH && plant.pH.length) {
+    process.stdout.write(`\x1b[1;37mpH:\x1b[0m ${plant.pH[0]} - ${plant.pH[1]}\n`);
+  }
+  if (plant.uptake && plant.uptake.length) {
+    process.stdout.write('\x1b[4;37mNutrient uptake\x1b[0m\n');
+    const table = {...data['conversions'], ...(plant.conversions || {})};
+    for (const nutrient of plant.uptake) {
+      const ratio = conversions.convertUptakeUnits(nutrient.rate, table);
+      process.stdout.write(`\x1b[1;37m${nutrient.nutrient}:\x1b[0m ${nutrient.rate} (${ratio})\n`);
+    }
   }
   console.log('');
 }
