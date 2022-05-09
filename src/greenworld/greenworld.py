99148223c34cfa874d8af25e6ab25602fcd58e3d
alugocp@aim.com <alugocp@aim.com>
2022-05-08 22:03:26 -0400

    Brainstormed some companionship rules

--- src/greenworld/greenworld.py
+++ src/greenworld/greenworld.py
@@ -1,13 +1,10 @@
-from typing import List
-from greenworld.model.species import Species
-from greenworld.math.comparisons import overlaps
 from greenworld.math.combinatorics import combination
+from greenworld.companionship import calculate_compatibility
 from greenworld.database.species import SpeciesData
 from greenworld.database.pair import PairData
 from greenworld.injector import Injector
-from greenworld.printer import Printer
-from greenworld.model.factor import Factor
 from greenworld.model.pair import Pair
+from greenworld.printer import Printer
 
 # This class is the main algorithm handler for calculating companionship pairs.
 # It ties all the business logic of this project together.
@@ -49,20 +46,3 @@ class Greenworld:
                 self.pair_data.write_pair(pair)
                 iteration += 1
         self.printer.close_stack()
-
-# This function calculates a compatibility report for the given pair of species.
-def calculate_compatibility(s1: Species, s2: Species) -> List[Factor]:
-    factors = []
-    if not overlaps(s1.pH, s2.pH):
-        factors.append(Factor(Factor.BAD,
-            f'{s1} and {s2} require different soil pH levels'
-        ))
-    if s1.drainage != s2.drainage:
-        factors.append(Factor(Factor.BAD,
-            f'{s1} and {s2} require different soil drainage levels'
-        ))
-    if s1.water != s2.water:
-        factors.append(Factor(Factor.NEUTRAL,
-            f'{s1} and {s2} will need different levels of water'
-        ))
-    return factors
