21cafda81fe6835321c2ec7340d3bef64f4f3e1f
alugocp <alugocp@aim.com>
2023-02-23 22:36:47 -0500

    Separated data entry logic into two files

--- greenworld/scripts/enter.py
+++ greenworld/scripts/enter.py
@@ -1,25 +1,12 @@
 import logging
 import copy
 import json
-import csv
 import sys
-import re
-import os
-import xlrd
 import sqlalchemy
 from sqlalchemy_utils import NumericRangeType
 from greenworld.lib import init_greenworld
 from greenworld.lib import schema
 from greenworld.lib import defs
-
-# CLI options structure for global access
-cli_options = {
-    'range': None,
-    'row-headers': None,
-    'col-headers': None,
-    'citation': None,
-    'map': {}
-}
 
 # Conversions table for internal standard units
 _conversions = {
@@ -79,133 +66,13 @@ def parse_enum(name):
     enum = name.split('.')
     try:
         return getattr(defs, enum[0])[enum[1]].value
-    except:
-        raise Exception(f'Unknown enum \'{name}\'')
-
-def parse_species_name(cells):
-    concatenated = ' '.join(cells).lower()
-    return re.sub(r' \([^)]+\)', '', concatenated)
-
-# Process plant table bulk data entry
-def enter_data(db, filename):
-    _, ext = os.path.splitext(filename)
-    print('')
-    logging.info('Writing data from %s...', filename)
-    if ext == '.json':
-        return enter_data_json(db, filename)
-    if ext == '.csv':
-        return enter_data_csv(db, filename)
-    if ext == '.xls':
-        return enter_data_xls(db, filename)
-    raise Exception(f'Data file with extension \'{ext}\' is not supported')
-
-# Process ecology data from XLS file
-def enter_data_xls(db, filename):
-    csv_filename = filename.replace('.xls', '.csv')
-    excel = xlrd.open_workbook(filename)
-    sheet = excel.sheet_by_index(0)
-    with open(csv_filename, 'w', encoding = 'utf-8') as file:
-        col = csv.writer(file)
-        for a in range(sheet.nrows):
-            col.writerow([cell.value for cell in sheet.row(a)])
-    enter_data_csv(db, csv_filename)
-
-# Process ecology data from CSV file
-def enter_data_csv(db, filename):
-    # Parse row and column headers
-    if cli_options['row-headers'] is not None and isinstance(cli_options['row-headers'][0], int):
-        cli_options['row-headers'] = extract_range_from_file(filename, cli_options['row-headers'])
-        cli_options['row-headers'] = list(map(parse_species_name, cli_options['row-headers']))
-    if cli_options['col-headers'] is not None and isinstance(cli_options['col-headers'][0], int):
-        cli_options['col-headers'] = extract_range_from_file(filename, cli_options['col-headers'])
-        cli_options['col-headers'] = list(map(parse_species_name, transposed(cli_options['col-headers'])))
-
-    # Extract interaction matrix data
-    if not cli_options['range']:
-        raise Exception(f'No range specified for data file \'{filename}\'')
-    data = extract_range_from_file(filename, cli_options['range'])
-    if not len(data) == len(cli_options['row-headers']):
-        raise Exception('Mismatched row headers length with number of data rows')
-    if not len(data[0]) == len(cli_options['col-headers']):
-        raise Exception('Mismatched column headers length with number of data columns')
-
-    # Write data to database
-    if not cli_options['citation']:
-        raise Exception(f'No citation provided for data file {filename}')
-    with db.connect() as con:
-        citation = retrieve_or_insert(con, schema.works_cited_table, 'citation', {
-            'citation': cli_options['citation']
-        })
-        for a, _ in enumerate(data):
-            for b, _ in enumerate(data[a]):
-                point = data[a][b]
-                if point in cli_options['map']:
-                    mapped = cli_options['map'][point]
-                    latin1 = cli_options['row-headers'][a]
-                    latin2 = cli_options['col-headers'][b]
-                    logging.info('Visiting %s x %s', latin1, latin2)
-                    species1 = select_by(con, schema.plants_table, 'species', latin1)
-                    if species1:
-                        species2_latin = latin2
-                    else:
-                        species1 = select_by(con, schema.plants_table, 'species', latin2)
-                        species2_latin = latin1
-                    if species1:
-                        species2 = retrieve_or_insert(con, schema.other_species_table, 'species', {
-                            'species': species2_latin,
-                            'name': species2_latin
-                        })
-                        interaction = {
-                            'plant': species1['id'],
-                            'non_plant': species2['id'],
-                            'relationship': mapped,
-                            'citation': citation['id']
-                        }
-                        con.execute(schema.ecology_other_table.insert().values(**interaction))
-                        logging.info(interaction)
-        con.commit()
-
-# Parses a line of a CSV file
-def parse_csv_line(line):
-    return list(csv.reader([line.rstrip()]))[0]
-
-# Parse a file range string
-def parse_range(text):
-    result = re.match(r'([0-9]+):([0-9]+),([0-9]+):([0-9]+)', text)
-    if not result:
-        return None
-    groups = list(result.groups())
-    for i, group in enumerate(groups):
-        groups[i] = None if group == '' else int(group)
-        if i % 2 == 0 and groups[i] is None:
-            groups[i] = 0
-    return groups
-
-# Extract a subset of a CSV file
-def extract_range_from_file(filename, r):
-    rows = []
-    with open(filename, 'r', encoding = 'utf-8') as file:
-        for i, row in enumerate(file.readlines()):
-            if r[2] <= i <= r[3]:
-                parsed = parse_csv_line(row)
-                rows.append(parsed[r[0] : r[1] + 1])
-    return rows
-
-# Grab headers as a list of lines from a file
-def extract_headers_from_file(filename):
-    with open(filename, 'r', encoding = 'utf-8') as file:
-        return file.read().rstrip().split('\n')
-
-# Returns a transposed matrix
-def transposed(matrix):
-    result = [[0 for _ in range(len(matrix))] for _ in range(len(matrix[0]))]
-    for a, _ in enumerate(matrix):
-        for b, _ in enumerate(matrix[a]):
-            result[b][a] = matrix[a][b]
-    return result
+    except Exception as e:
+        raise Exception(f'Unknown enum \'{name}\'') from e
 
 # Process plant table bulk data entry for a JSON file
-def enter_data_json(db, filename):
+def enter_data(db, filename):
+    print('')
+    logging.info('Writing data from %s...', filename)
     with open(filename, 'r', encoding = 'utf-8') as file:
         data = json.load(file)
     with db.connect() as con:
@@ -307,29 +174,11 @@ def process_ecological_fields(con, works_cited_map, plant_id, data):
 def main(args):
     init_greenworld()
     db = schema.init_db()
-    a = 0
-    while a < len(args):
+    for a, _ in enumerate(args):
         if args[a] == '--help':
             print_help()
             break
-        if '--' in args[a] and args[a].index('--') == 0:
-            if not len(args) > a + 2:
-                raise Exception('Not enough CLI arguments')
-            key = args[a][2:]
-            if not key in cli_options:
-                raise Exception(f'Unknown CLI option \'{key}\'')
-            if key == 'map':
-                cli_options['map'][args[a + 1]] = parse_enum('Ecology.' + args[a + 2])
-            elif key == 'range':
-                cli_options['range'] = parse_range(args[a + 2])
-            elif key in ['row-headers', 'col-headers']:
-                cli_options[key] = parse_range(args[a + 2]) or extract_headers_from_file(args[a + 2])
-            else:
-                cli_options[key] = args[a + 2]
-            a += 3
-        else:
-            enter_data(db, args[a])
-            a += 1
+        enter_data(db, args[a])
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- greenworld/scripts/reset.py
+++ greenworld/scripts/reset.py
@@ -1,6 +1,7 @@
 # This script hard resets the local database used in this project.
 # It then repopulates the database from the `seed.json` file.
 import logging
+from greenworld.collection import iweb_xls
 from greenworld.scripts import enter
 from greenworld.lib import init_greenworld
 from greenworld.lib import schema
@@ -33,7 +34,7 @@ def main():
         'seed-data/native-plants.json',
         'seed-data/pollinators.json'
     ])
-    enter.main(
+    iweb_xls.main(
         '--citation = https://iwdb.nceas.ucsb.edu/html/clements_1923.html --col-headers = 4:100,1:2 --row-headers = 1:2,4:279 --range = 4:100,4:279 --map 1.0 POLLINATOR referenced-data/clements_1923.xls'.split(' ')
     )
 
