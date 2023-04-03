85b62471b10869d9bc7d4bd9f469839481844f6f
alugocp <alugocp@aim.com>
2023-04-02 22:05:26 -0400

    TypeScript linter/formatter

--- Makefile
+++ Makefile
@@ -5,14 +5,19 @@ COMMA:= ,
 EMPTY:=
 SPACE:= $(EMPTY) $(EMPTY)
 
-lint:
+lint: lint-py lint-ts
+
+lint-py:
 	$(ENV) $(PYTHON) -m pylint $(shell git ls-files "*.py")
+
+lint-ts:
+	$(NPM_BIN)/eslint greenworld/server/src
 
 install:
 	$(PYTHON) -m pip install -r requirements.txt
 
 ui:
-	$(NPM_BIN)/tsc -p greenworld/server/tsconfig.json
+	$(NPM_BIN)/tsc -p tsconfig.json
 
 serve: ui
 	$(ENV) $(PYTHON) greenworld/server/app.py
--- greenworld/server/src/script.ts
+++ greenworld/server/src/script.ts
@@ -1,27 +1,31 @@
 
-const cachedData = window as unknown as {
-    greenworld_cached_html: string;
-    greenworld_content: string;
+const cachedData = {
+    greenworld_cached_html: '',
+    greenworld_content: ''
 };
 
 // Triggers the search button if the enter key is pressed
-function searchBarOnKeyDown(e: KeyboardEvent, plantToUrl: (_: string) => string) {
+export function searchBarOnKeyDown(e: KeyboardEvent, plantToUrl: (_: string) => string): void {
+    const baseUrl: string = (window as any).gw.base_url as string;
     if (e.keyCode === 13) {
         e.preventDefault();
-        document.getElementById('search-button').click();
+        document.getElementById('search-button')?.click();
     } else {
         const content: string = (document.getElementById('search-field') as HTMLInputElement).value;
-        const results: HTMLElement = document.getElementById('search-results');
+        const results: HTMLElement | null = document.getElementById('search-results');
+        if (results === null) {
+            return;
+        }
         if (content.length >= 3) {
             if (cachedData.greenworld_content === content) {
                 results.innerHTML = cachedData.greenworld_cached_html;
             } else {
                 cachedData.greenworld_content = content;
-                fetch(`{{ url_for('homepage_endpoint') }}search/${content}`)
-                    .then(r => r.json())
+                void fetch(`${baseUrl}search/${content}`)
+                    .then((r) => r.json())
                     .then((data) => {
                         cachedData.greenworld_cached_html = data
-                            .map((plant: any): string => `<a href = "{{ url_for('homepage_endpoint') }}${plantToUrl(plant[0])}">${plant[1]} (${plant[0]})</a>`)
+                            .map((plant: any): string => `<a href = "${baseUrl}${plantToUrl(plant[0] as string)}">${plant[1] as string} (${plant[0] as string})</a>`)
                             .reduce((acc: string, x: string): string => acc + x, '');
                         results.innerHTML = cachedData.greenworld_cached_html;
                     });
@@ -33,7 +37,8 @@ function searchBarOnKeyDown(e: KeyboardEvent, plantToUrl: (_: string) => string)
 }
 
 // Navigates to a page page by the entered text
-function navigate(plantToUrl: (_: string) => string) {
+export function navigate(plantToUrl: (_: string) => string): void {
+    const baseUrl: string = (window as any).gw.base_url as string;
     const input: HTMLInputElement = document.getElementById('search-field') as HTMLInputElement;
-    window.location.assign(`{{ url_for('homepage_endpoint') }}${plantToUrl(input.value)}`);
+    window.location.assign(`${baseUrl}${plantToUrl(input.value)}`);
 }
