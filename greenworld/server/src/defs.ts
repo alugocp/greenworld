b0b4a8165a691bca9dbf82bf62d453b0a1e258df
alugocp <alugocp@aim.com>
2023-04-04 23:30:04 -0400

    Algorithm extension for n > 3 guilds

--- greenworld/server/src/defs.ts
+++ greenworld/server/src/defs.ts
@@ -19,8 +19,14 @@ export interface Point {
     y: number
 }
 
+export interface Edge {
+    p1: number
+    p2: number
+    dist: number
+}
+
 export interface Guild {
     plants: Array<PlantHandle & Point & { r: number }>
-    edges: Array<{ p1: number, p2: number, dist: number }>
     bounds: { upperLeft: Point, lowerRight: Point }
+    edges: Edge[]
 }
--- greenworld/server/src/guild.ts
+++ greenworld/server/src/guild.ts
@@ -1,4 +1,4 @@
-import type { PlantHandle, Report, Point, Guild } from './defs';
+import type { PlantHandle, Report, Point, Guild, Edge } from './defs';
 
 // Grab report between two plant species
 function getReport(reports: Report[], plant1: PlantHandle, plant2: PlantHandle): Report {
@@ -17,23 +17,50 @@ function commonPlant(r1: Report, r2: Report): number {
     return r1.plant1 === r2.plant1 || r1.plant1 === r2.plant2 ? r1.plant1 : r1.plant2;
 }
 
+// Returns true if the two edges are equivalent
+function edgesEqual(e1: Edge, e2: Edge): boolean {
+    return ((e1.p1 === e2.p1 && e1.p2 === e2.p2) || (e1.p1 === e2.p2 && e1.p2 === e2.p1)) && e1.dist === e2.dist;
+}
+
+// Grabs a common edge between two guilds
+function grabCommonEdge(g1: Guild, g2: Guild): Edge | null {
+    for (const e1 of g1.edges) {
+        for (const e2 of g2.edges) {
+            if (edgesEqual(e1, e2)) {
+                return e1;
+            }
+        }
+    }
+    return null;
+}
+
 // Initializes the guild finder module
 export async function init(canvas: HTMLCanvasElement): Promise<void> {
     const plants: PlantHandle[] = [
         {
-            id: 1,
+            id: 95,
             name: 'Hopi Turquoise Corn',
             species: 'zea mays'
         },
         {
-            id: 2,
+            id: 96,
             name: 'Hopi Orange Squash',
             species: 'cucurbita maxima'
         },
         {
-            id: 3,
+            id: 97,
             name: 'Hopi Purple String Bean',
             species: 'phaseolus vulgaris'
+        },
+        {
+            id: 98,
+            name: 'Hopi Black Dye Sunflower',
+            species: 'helianthus annuus'
+        },
+        {
+            id: 99,
+            name: 'Purple Passionflower',
+            species: 'passiflora incarnata'
         }
     ];
 
@@ -46,21 +73,94 @@ export async function init(canvas: HTMLCanvasElement): Promise<void> {
             return { ...r, range_union_min: parseFloat(`${r.range_union_min}`), range_union_max: parseFloat(`${r.range_union_max}`) };
         }));
 
-    // Triangulation algorithm
+    // Build n > 2 guild
     const guild: Guild | null = findTriangle(
         plants,
         getReport(reports, plants[0], plants[1]),
         getReport(reports, plants[1], plants[2]),
         getReport(reports, plants[0], plants[2])
     );
+    if (guild === null) {
+        return;
+    }
     console.log(guild);
+    for (let a = 3; a < plants.length; a++) {
+        const plantC = plants[a];
+        for (const e of guild.edges) {
+            const plantA: PlantHandle = getPlantHandle(plants, e.p1);
+            const plantB: PlantHandle = getPlantHandle(plants, e.p2);
+            const newGuild: Guild | null = findTriangle(
+                plants,
+                getReport(reports, plantA, plantB),
+                getReport(reports, plantA, plantC),
+                getReport(reports, plantB, plantC)
+            );
+            if (newGuild === null) {
+                continue;
+            }
+            const commonEdge: Edge | null = grabCommonEdge(guild, newGuild);
+            if (commonEdge === null) {
+                continue;
+            }
+            const plantPoint = getPlantHandle(newGuild.plants, plantC.id);
+            const dx = getPlantHandle(guild.plants, plantA.id).x - getPlantHandle(newGuild.plants, plantA.id).x;
+            const dy = getPlantHandle(guild.plants, plantA.id).y - getPlantHandle(newGuild.plants, plantA.id).y;
+            const b1x = getPlantHandle(guild.plants, plantB.id).x;
+            const b1y = getPlantHandle(guild.plants, plantB.id).y;
+            const b2x = getPlantHandle(newGuild.plants, plantB.id).x + dx;
+            const b2y = getPlantHandle(newGuild.plants, plantB.id).y + dy;
+            const l = commonEdge.dist;
+            const theta = Math.acos((2 * Math.pow(l, 2) - Math.pow(distance(b1x - b2x, b1y - b2y), 2)) / (2 * Math.pow(l, 2)));
+            console.log({
+                commonEdge,
+                dx,
+                dy,
+                b1x,
+                b1y,
+                b2x,
+                b2y,
+                l,
+                theta,
+                almost: (2 * Math.pow(l, 2) - Math.pow(distance(b1x - b2x, b1y - b2y), 2)) / (2 * Math.pow(l, 2))
+            });
+            const result: Point = rotateAbout(
+                { x: plantPoint.x + dx, y: plantPoint.y + dy },
+                getPlantHandle(guild.plants, plantA.id),
+                theta
+            );
+            console.log(result);
+            let tooClose = false;
+            const edgesToAdd: Edge[] = [];
+            for (const p of guild.plants) {
+                if (p === plantA || p === plantB) {
+                    continue;
+                }
+                const d = distance(result.x - p.x, result.y - p.y);
+                if (d < getReport(reports, p, plantC).range_union_min) {
+                    tooClose = true;
+                    break;
+                }
+                edgesToAdd.push({ p1: Math.min(plantC.id, p.id), p2: Math.max(plantC.id, p.id), dist: d });
+            }
+            if (tooClose) {
+                continue;
+            }
+            guild.plants.push({ ...plantC, ...result, r: 0.025 });
+            guild.edges = guild.edges.concat(newGuild.edges.filter((e: Edge) => e.p1 === plantC.id || e.p2 === plantC.id)).concat(edgesToAdd);
+            guild.bounds.upperLeft.x = Math.min(guild.bounds.upperLeft.x, result.x - 0.05);
+            guild.bounds.upperLeft.y = Math.min(guild.bounds.upperLeft.y, result.y - 0.05);
+            guild.bounds.lowerRight.x = Math.max(guild.bounds.lowerRight.x, result.x + 0.05);
+            guild.bounds.lowerRight.y = Math.max(guild.bounds.lowerRight.y, result.y + 0.05);
+            break;
+        }
+    }
 
     // Draw to canvas
     draw(canvas, guild);
 }
 
 // Draws guild representation to the canvas
-function draw(canvas: HTMLCanvasElement, guild: Guild | null): void {
+function draw(canvas: HTMLCanvasElement, guild: Guild): void {
     const ctx = canvas.getContext('2d');
     if (ctx === null) {
         return;
@@ -71,9 +171,6 @@ function draw(canvas: HTMLCanvasElement, guild: Guild | null): void {
     ctx.strokeStyle = '#000000';
     ctx.font = '20px sans-serif';
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (guild === null) {
-        return;
-    }
     const labels: Array<Point & { text: string }> = [];
     const dx = -guild.bounds.upperLeft.x;
     const dy = -guild.bounds.upperLeft.y;
@@ -82,7 +179,7 @@ function draw(canvas: HTMLCanvasElement, guild: Guild | null): void {
         canvas.height / (guild.bounds.lowerRight.y - guild.bounds.upperLeft.y)
     );
     ctx.lineWidth = scale / 320;
-    for (const e of guild.edges) {
+    /* for (const e of guild.edges) {
         const p1 = getPlantHandle(guild.plants, e.p1);
         const p2 = getPlantHandle(guild.plants, e.p2);
         ctx.beginPath();
@@ -94,7 +191,7 @@ function draw(canvas: HTMLCanvasElement, guild: Guild | null): void {
             x: (p1.x + dx + p2.x + dx) * scale / 2,
             y: (p1.y + dy + p2.y + dy) * scale / 2
         });
-    }
+    } */
     for (const p of guild.plants) {
         ctx.beginPath();
         ctx.arc((p.x + dx) * scale, (p.y + dy) * scale, p.r * scale, 0, Math.PI * 2);
@@ -115,6 +212,16 @@ function draw(canvas: HTMLCanvasElement, guild: Guild | null): void {
 // Pythagorean distance formula
 function distance(x: number, y: number): number {
     return Math.round(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) * 1000) / 1000;
+}
+
+// Rotates point p about origin by theta
+function rotateAbout(p: Point, origin: Point, dtheta: number): Point {
+    const theta = Math.atan2(p.y - origin.y, p.x - origin.x) + dtheta;
+    const dist = distance(p.x - origin.x, p.y - origin.y);
+    return {
+        x: origin.x + Math.cos(theta) * dist,
+        y: origin.y + Math.sin(theta) * dist
+    };
 }
 
 // Builds a triangle guild from the three plants and side lengths
@@ -164,9 +271,9 @@ function findTriangle(plants: PlantHandle[], p12: Report, p23: Report, p13: Repo
     }
 
     // Pick the min value of each range
-    const l1: number = r1.range_union_min ? r1.range_union_min : Math.min(0.16, r1.range_union_max);
-    let l2: number = r2.range_union_min ? r2.range_union_min : Math.min(0.16, r2.range_union_max);
-    let l3: number = r3.range_union_min ? r3.range_union_min : Math.min(0.16, r3.range_union_max);
+    const l1: number = r1.range_union_min !== 0 ? r1.range_union_min : Math.min(0.16, r1.range_union_max);
+    let l2: number = r2.range_union_min !== 0 ? r2.range_union_min : Math.min(0.16, r2.range_union_max);
+    let l3: number = r3.range_union_min !== 0 ? r3.range_union_min : Math.min(0.16, r3.range_union_max);
 
     // If it's already a triangle then return that
     if (l2 + l3 > l1) {
