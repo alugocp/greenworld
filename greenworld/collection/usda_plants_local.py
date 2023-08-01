af3274b7fd0bf85a102df6ccffa4a39c4ccaf9db
alugocp <alugocp@aim.com>
2023-08-01 18:40:35 -0400

    More effective plant common name lookup

--- greenworld/collection/usda_plants_local.py
+++ greenworld/collection/usda_plants_local.py
@@ -1,29 +1,39 @@
-from typing import List
+from typing import Dict, List
 import re
 import csv
 from greenworld.collection import BaseDataCollector
 from greenworld.lib import Greenworld
 
 class UsdaPlantsLocalDataCollector(BaseDataCollector):
-    __database: List[str] = []
+    __genera: Dict[str, List[str]] = {}
+    __families: Dict[str, str] = {}
 
     def __init__(self, gw: Greenworld):
         super().__init__(gw)
         with open('referenced-data/usda_plants_database.txt', 'r', encoding = 'utf-8') as file:
             for line in file.readlines():
-                sanitized = re.fullmatch(r'^"[A-Z0-9]*","[A-Z0-9]*","([^\n]+)\n?$', line)
-                if sanitized:
-                    self.__database.append(sanitized.group(1).lower())
+                sanitized = list(csv.reader([line]))[0]
+                common = sanitized[3]
+                if len(common) > 0:
+                    genus = sanitized[2].lower().split(' ')[0]
+                    family = sanitized[4].lower()
+                    if genus not in self.__families:
+                        self.__families[genus] = family
+                    if genus not in self.__genera:
+                        self.__genera[genus] = []
+                    self.__genera[genus].append([sanitized[2].lower(), common])
 
     # TODO rewrite this function with binary search algorithm (data is in alphabetical order)
     def find_match(self, species: str) -> dict:
-        for line in self.__database:
-            if line.startswith(species):
-                row = list(csv.reader([f'"{line}']))[0]
-                return {
-                    'name': row[1].title().replace('\'S', '\'s'),
-                    'family': row[2]
-                }
+        genus = species.split(' ')[0]
+        if genus in self.__genera:
+            suffix = ' '.join(species.split(' ')[1:])
+            for entry in self.__genera[genus]:
+                if suffix in entry[0]:
+                    return {
+                        'name': entry[1],
+                        'family': self.__families[genus]
+                    }
         return None
 
     def matches_input(self, key: str) -> bool:
