0af5a61008919593eeba53e37f40250a1ebf23f5
alugocp <alugocp@aim.com>
2023-08-01 13:03:06 -0400

    Removed something I forgot

--- frontend/wrapper.ts
+++ frontend/wrapper.ts
@@ -1,8 +1,5 @@
 
 export default class UiWrapper {
-    // App-specific init method
-    init(): void {}
-
     // Wrapper for the window's fetch function
     async fetch(url: string): Promise<any> {}
 }
